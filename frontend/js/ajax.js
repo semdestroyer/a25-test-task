a0d99c3d7112fc897c038f5a7ae5585cfbd2a99f
Arsenii <semdestroyer@yandex.ru>
2021-07-21 22:11:39 +0300

    full app complete backend + front

--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -1,4 +1,15 @@
-function sendl()
+function send()
 {
-
+    data = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+    }
+    
+    fetch("http://127.0.0.1:8000/mail",{method:"POST", 
+    mode: 'no-cors',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)});
 }
